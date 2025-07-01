b5a04381204cb4d2f9ca1157f35a38ddd8a3e09a
Khalfan AlShamsi <khalfanalshamsi02@gmail.com>
2025-07-01 11:47:47 +0400

    implemented basic game loop

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 # This allows us to use code from the open source pygame library throughout this file
-import pygame
+import pygame as pg
+from constants import *
 
 def main():
     print("Starting Asteroids!")
-
+    print(f"Screen width: {SCREEN_WIDTH}\nScreen height: {SCREEN_HEIGHT}")
+    pg.init()
+    screen = pg.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                return
+        screen.fill("black")
+        pg.display.flip()
 
 if __name__ == "__main__":
     main()
